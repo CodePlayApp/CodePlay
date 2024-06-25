456a56ef0de792c0ea65cf8264b4e06570c40860
shabman <avia.shabbyman@gmail.com>
2024-06-25 18:17:54 +0100

    Initial Commit

---
+++ CodePlay/CodePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePlayApp.swift
+//  CodePlay
+//
+//  Created by Mustafa Malik on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CodePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
